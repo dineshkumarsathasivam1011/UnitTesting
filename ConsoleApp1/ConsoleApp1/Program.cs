a7f90ec013cc16441d192654d34928e8ec40c15e
dineshkumarsathasivam1011 <sathasivamdineshkumar@gmail.com>
2025-08-09 18:41:21 +0530

    The unit testing committ

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter the string:");
+        string input = Console.ReadLine();
+        Dictionary<char, int> letterCount = new Dictionary<char, int>();
+
+        foreach (char c in input)
+        {
+            if (char.IsLetter(c))
+            {
+                if (letterCount.ContainsKey(c))
+                {
+                    letterCount[c]++;
+                }
+                else
+                {
+                    letterCount[c] = 1;
+                }
+
+            }
+
+            Console.WriteLine("LetterOccurance:");
+
+            foreach (var kvp in letterCount)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value}");
+            }
+
+        }
+        
+    }
+
+}
---
+++ WebApplication1/WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet("test/{id}")]
+
+        Route("test/{id}")]
+
+        public  IActionResult GetDetails(int id)
+        {
+            // Simulate fetching data based on the id
+            var data = new { Id = id, Name = "Test Item", Description = "This is a test item." };
+            // Return the data as JSON
+           // return Json(data)
+
+            return Ok(data);
+        }
+}
